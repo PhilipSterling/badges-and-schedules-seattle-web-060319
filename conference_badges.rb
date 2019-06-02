c9f6a3861b1c3c14c56ce2cdf066d88f98ae8de9
Philip Sterling <philipssterlings@gmail.com>
2019-06-02 04:35:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,8 @@
 # Write your code here.
 def badge_maker(name)
-  string = 
+  string = "Hello, my name is #{name}."
+  return string
+end
+def badge_batch_creator(array)
+  array.each |x| badge_maker(x)
+end
